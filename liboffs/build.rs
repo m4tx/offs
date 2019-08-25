9def26af9fe652aaf6c5299e7cf0c71a74beb9b1
Mateusz Maćkowski <m4tx@m4tx.pl>
2019-08-25 11:32:26 +0200

    Add initial gRPC proto

--- liboffs/build.rs
+++ liboffs/build.rs
@@ -3,8 +3,15 @@ extern crate capnpc;
 fn main() {
     ::capnpc::CompilerCommand::new()
         .src_prefix("src/proto")
-        .edition(::capnpc::RustEdition::Rust2018)
         .file("src/proto/filesystem.capnp")
         .run()
         .expect("schema compiler command");
+
+    let proto_root = "src/proto";
+    protoc_grpcio::compile_grpc_protos(
+        &["filesystem.proto"],
+        &[proto_root],
+        &proto_root,
+        None
+    ).expect("Failed to compile gRPC definitions");
 }
